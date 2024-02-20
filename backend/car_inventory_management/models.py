9b907fd75bddff36e378c5ae44cfda33cd6b1067
Vachan Nadupalli <vachan.nadupalli@infinitylearn.com>
2024-02-20 23:23:55 +0530

    Added super user creation

--- backend/car_inventory_management/models.py
+++ backend/car_inventory_management/models.py
@@ -3,6 +3,32 @@ from datetime import datetime
 from django.contrib.auth.models import AbstractUser
 
 
+from django.contrib.auth.models import BaseUserManager
+from django.utils import timezone
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
 class CustomUser(AbstractUser):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, unique=True)
@@ -14,6 +40,8 @@ class CustomUser(AbstractUser):
     username = None
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
 
 class CarsModel(models.Model):
